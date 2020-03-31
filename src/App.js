79ec175b5cd76f0fa903ec19b5982b111ff8feae
Brianna Workman <brianna.workman@hotmail.com>
2020-03-30 23:00:38 -0500

    adds australia and canada data

--- src/App.js
+++ src/App.js
@@ -7,10 +7,15 @@ import TopCards from './Components/TopCards';
 import PieChartComponent from './Components/PieChart';
 import RadialChart from './Components/RadialChart';
 import USChart from './Components/USChart';
+import CanadaChart from './Components/CanadaChart';
+import AustraliaChart from './Components/AustraliaChart';
 
 function App() {
   const [global, setGlobal] = useState([]);
   const [usa, setUsa] = useState([]);
+  const [canada, setCanada] = useState([]);
+  const [australia, setAustralia] = useState([]);
+
   useEffect(() => {
     axios
       .get(`https://api.covid19api.com/summary`)
@@ -22,12 +27,28 @@ function App() {
       });
 
     axios
-      .get(
-        `https://corona.lmao.ninja/states
-`
-      )
+      .get(`https://corona.lmao.ninja/states`)
       .then(response => {
         setUsa(response.data);
+      })
+      .catch(error => {
+        console.log('The data was not returned', error);
+      });
+
+    axios
+      .get(`https://cors-anywhere.herokuapp.com/https://bing.com/covid/data`)
+      .then(response => {
+        setCanada(response.data.areas[14].areas);
+      })
+      .catch(error => {
+        console.log('The data was not returned', error);
+      });
+
+    axios
+      .get(`https://cors-anywhere.herokuapp.com/https://bing.com/covid/data`)
+      .then(response => {
+        console.log(response.data.areas);
+        setAustralia(response.data.areas[17].areas);
       })
       .catch(error => {
         console.log('The data was not returned', error);
@@ -42,6 +63,8 @@ function App() {
           <TopCards global={global} />
           <GlobalChart global={global} />
           <USChart usa={usa} />
+          <CanadaChart canada={canada} />
+          <AustraliaChart australia={australia} />
         </div>
         <div className='other-data'>
           <PieChartComponent global={global} />
